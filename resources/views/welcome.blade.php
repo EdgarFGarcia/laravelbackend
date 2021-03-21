b8dbb1d447f80732412712a5cfedcd91999ac6cf
unknown <edgarfrancisgarcia7@gmail.com>
2021-03-21 16:29:01 +0800

    update readme.md

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,6 +22,9 @@
     </head>
     <body class="antialiased">
         You Will Receive an email shortly. Thank you!
+        <?php
+            EcHo "meow";
+        ?>
         <script
         src="https://code.jquery.com/jquery-3.6.0.js"
         integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="
